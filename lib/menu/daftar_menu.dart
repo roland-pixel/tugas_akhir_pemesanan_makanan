aa2cf488402116de634d6091007c5dedb6550850
roland-pixel <kharis7181@gmail.com>
2024-12-18 21:06:11 +0700

    widget scann, wdget reward

--- lib/menu/daftar_menu.dart
+++ lib/menu/daftar_menu.dart
@@ -26,32 +26,131 @@ class DaftarMenuPage extends StatelessWidget {
         backgroundColor: Colors.black87,
         title: Row(
           children: [
-            Image.asset(
-              'assets/images/logo_mcd.png',
-              width: 40,
-              height: 40,
-            ),
-            const SizedBox(width: 10),
             const Text(
-              'MyM',
+              'My',
               style: TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
                 color: Colors.yellow,
               ),
             ),
+            const SizedBox(width: 5),
+            Image.asset(
+              'assets/images/logo_mcd.png',
+              width: 40,
+              height: 40,
+            ),
             const Spacer(),
-            const Text(
-              '0 poin',
-              style: TextStyle(
-                fontSize: 16,
-                color: Colors.white,
+            InkWell(
+              onTap: () {},
+              child: Row(
+                children: [
+                  const Text(
+                    '0 poin',
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  const Icon(
+                    Icons.arrow_forward_ios,
+                    color: Colors.white,
+                    size: 15,
+                  ),
+                ],
               ),
             ),
-            const Icon(Icons.arrow_forward_ios),
           ],
         ),
-      ), // Panggil fungsi AppBar
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          SizedBox(height: 10),
+          Center(
+            child: Container(
+              padding: EdgeInsets.all(16),
+              color: Colors.yellow,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(8),
+                    child: Column(
+                      children: [
+                        Image.asset(
+                          'assets/images/qrcode.png',
+                          width: 300,
+                          height: 150,
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Text(
+                          'M 189 016',
+                          style: TextStyle(
+                              fontSize: 16, fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16,
+                  ),
+                  Text(
+                    'Pindai kode untuk mendapatkan Poin.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Reward',
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Row(
+                  children: [
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        children: [
+                          Text(
+                            'Lihat Semua',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.blue[900],
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Icon(
+                            Icons.arrow_forward_outlined,
+                            color: Colors.blue[900],
+                            size: 16,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
