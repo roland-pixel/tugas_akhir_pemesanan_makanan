f85743bed989fc9e15ae77d26e58cb07c64298d1
daywsenada <daywsenadaart@gmail.com>
2025-01-12 13:11:41 +0800

    d

--- lib/pembayaran/pembayaran.dart
+++ lib/pembayaran/pembayaran.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: PaymentPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
@@ -19,7 +20,7 @@ class PaymentPage extends StatefulWidget {
 }
 
 class _PaymentPageState extends State<PaymentPage> {
-  String _selectedPaymentMethod;
+  String _selectedPaymentMethod = '';
   List<Map<String, dynamic>> orderItems = [
     {'name': 'Nasi Goreng', 'price': 25000},
     {'name': 'Mie Ayam', 'price': 20000},
@@ -31,11 +32,19 @@ class _PaymentPageState extends State<PaymentPage> {
   int foodDiscount = 20000;
 
   int get subtotal {
-    return orderItems.fold(0, (sum, item) => sum + item['price']);
+    return orderItems.fold(0, (sum, item) {
+      int price = item['price'] ?? 0; // Pastikan harga valid
+      return sum + price;
+    });
   }
 
   int get totalPrice {
-    return subtotal + deliveryFee + serviceFee + additionalFee - shippingDiscount - foodDiscount;
+    return subtotal +
+        deliveryFee +
+        serviceFee +
+        additionalFee -
+        shippingDiscount -
+        foodDiscount;
   }
 
   @override
@@ -45,103 +54,149 @@ class _PaymentPageState extends State<PaymentPage> {
         title: Text('Pembayaran'),
       ),
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Ringkasan Pesanan',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 10),
-            Expanded(
-              child: ListView.builder(
-                itemCount: orderItems.length,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    title: Text(orderItems[index]['name']),
-                    trailing: Text('Rp ${orderItems[index]['price']}'),
-                  );
-                },
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                'Ringkasan Pesanan',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
-            ),
-            Divider(),
-            Text(
-              'Subtotal untuk 2 item: Rp $subtotal',
-              style: TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Biaya Pengiriman: Rp $deliveryFee',
-              style: TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Biaya Layanan: Rp $serviceFee',
-              style: TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Biaya Tambahan Restoran: Rp $additionalFee',
-              style: TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Diskon Pengiriman: -Rp $shippingDiscount',
-              style: TextStyle(fontSize: 16),
-            ),
-            Text(
-              'Diskon Makanan: -Rp $foodDiscount',
-              style: TextStyle(fontSize: 16),
-            ),
-            Divider(),
-            Text(
-              'Total Pembayaran: Rp $totalPrice',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 20),
-            Text(
-              'Metode Pembayaran',
-              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-            RadioListTile(
-              title: Text('Tunai saat pengambilan'),
-              value: 'cash',
-              groupValue: _selectedPaymentMethod,
-              onChanged: (value) {
-                setState(() {
-                  _selectedPaymentMethod = value;
-                });
-              },
-            ),
-            RadioListTile(
-              title: Text('Transfer Bank'),
-              value: 'bank_transfer',
-              groupValue: _selectedPaymentMethod,
-              onChanged: (value) {
-                setState(() {
-                  _selectedPaymentMethod = value;
-                });
-              },
-            ),
-            RadioListTile(
-              title: Text('E-Wallet'),
-              value: 'e_wallet',
-              groupValue: _selectedPaymentMethod,
-              onChanged: (value) {
-                setState(() {
-                  _selectedPaymentMethod = value;
-                });
-                },
-            ),
-            SizedBox(height: 20),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  // Logika pembayaran bisa ditambahkan di sini
-                  print('Pesanan dibayar dengan $_selectedPaymentMethod');
-                },
-                child: Text('Bayar'),
+              SizedBox(height: 10),
+              _buildOrderSummary(),
+              Divider(),
+              _buildPriceDetails(),
+              Divider(),
+              Text(
+                'Total Pembayaran: Rp $totalPrice',
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              Text(
+                'Metode Pembayaran',
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+              _buildPaymentOptions(),
+              SizedBox(height: 20),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (_selectedPaymentMethod.isEmpty) {
+                      _showErrorDialog(
+                          context, 'Silakan pilih metode pembayaran.');
+                    } else {
+                      _showSuccessDialog(context);
+                    }
+                  },
+                  child: Text('Bayar'),
+                ),
+              ),
+            ],
+          ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildOrderSummary() {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      itemCount: orderItems.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(orderItems[index]['name']),
+          trailing: Text('Rp ${orderItems[index]['price']}'),
+        );
+      },
+    );
+  }
+
+  Widget _buildPriceDetails() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text('Subtotal untuk ${orderItems.length} item: Rp $subtotal',
+            style: TextStyle(fontSize: 16)),
+        Text('Biaya Pengiriman: Rp $deliveryFee',
+            style: TextStyle(fontSize: 16)),
+        Text('Biaya Layanan: Rp $serviceFee', style: TextStyle(fontSize: 16)),
+        Text('Biaya Tambahan Restoran: Rp $additionalFee',
+            style: TextStyle(fontSize: 16)),
+        Text('Diskon Pengiriman: -Rp $shippingDiscount',
+            style: TextStyle(fontSize: 16)),
+        Text('Diskon Makanan: -Rp $foodDiscount',
+            style: TextStyle(fontSize: 16)),
+      ],
+    );
+  }
+
+  Widget _buildPaymentOptions() {
+    return Column(
+      children: [
+        RadioListTile(
+          title: Text('Tunai saat pengambilan'),
+          value: 'cash',
+          groupValue: _selectedPaymentMethod,
+          onChanged: (value) {
+            setState(() {
+              _selectedPaymentMethod = value!;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('Transfer Bank'),
+          value: 'bank_transfer',
+          groupValue: _selectedPaymentMethod,
+          onChanged: (value) {
+            setState(() {
+              _selectedPaymentMethod = value!;
+            });
+          },
+        ),
+        RadioListTile(
+          title: Text('E-Wallet'),
+          value: 'e_wallet',
+          groupValue: _selectedPaymentMethod,
+          onChanged: (value) {
+            setState(() {
+              _selectedPaymentMethod = value!;
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  void _showErrorDialog(BuildContext context, String message) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Error'),
+        content: Text(message),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _showSuccessDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: Text('Berhasil'),
+        content: Text('Pesanan dibayar dengan $_selectedPaymentMethod.'),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.pop(context),
+            child: Text('OK'),
+          ),
+        ],
       ),
     );
   }
